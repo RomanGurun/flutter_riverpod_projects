7b71fc5e1389b0730e0e64f2ad537fa0537222df
roman <romangrg569@gmail.com>
2026-01-06 08:51:30 -0800

    Initial  commit

--- lib/providers/cart_provider.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../screens/home_page.dart';
-import 'cart_notifier.dart';
-
-final cartProvider = ChangeNotifierProvider((ref) {
-  return CartNotifier();
-});
--- lib/providers/timer_provider.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-final timerProvider = StreamProvider((ref) {
-  return Stream.periodic(
-    Duration(seconds: 1),
-        (count) => count,
-  );
-});
