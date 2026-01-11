e46f66a2fc0f9cc1676d121bde3dad02b03ebfff
roman <romangrg569@gmail.com>
2026-01-11 00:53:07 -0800

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'screens/home_page.dart';
+import 'package:login/views/screens/todo_list_screen.dart';
+import 'services/hive_service.dart';
+import 'views/screens/todo_detail_screen.dart';
 
-void main() {
-  runApp(
-    ProviderScope(
-      child: MyApp(),
-    ),
-  );
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final hive = HiveService();
+  await hive.init();
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Riverpod 2.0 Examples',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        brightness: Brightness.light,
-      ),
-      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(useMaterial3: true, colorSchemeSeed: Colors.indigo),
+      home: const TodoListScreen(),
     );
   }
 }
